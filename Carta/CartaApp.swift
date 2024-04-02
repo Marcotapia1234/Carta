b5aeb2851232b25921ac9f66e884be16e94d98c6
Marco <empleomarcotapia@gmail.com>
2024-04-02 06:22:03 -0700

    Initial Commit

---
+++ Carta/CartaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartaApp.swift
+//  Carta
+//
+//  Created by Marco on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CartaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
